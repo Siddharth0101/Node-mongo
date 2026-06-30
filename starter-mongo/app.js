17c48006603dba6a1c6eca9fa7b5e3e05081b6e0
Siddharth0101 <siddsikarwar2001@gmail.com>
2026-06-30 11:36:18 +0530

    added routes

--- starter-mongo/app.js
+++ starter-mongo/app.js
@@ -1,13 +1,49 @@
 const express = require('express')
-
+const fs = require('fs')
+const morgan = require('morgan')
 const app = express()
 
-app.get('/',(req,res)=>{
-    res.status(200).json({status:'success',message:'hello from the server'})
-})
+app.use(morgan('dev'))
+
+// express.json() incoming JSON request body ko parse karta hai taaki req.body use kar sakein
+app.use(express.json())
+
+// app.get('/',(req,res)=>{
+//     res.status(200).json({status:'success',message:'hello from the server'})
+// })
+
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
+
+const getAllTours = (req, res) => {
+    res.status(200).json({
+        status: 'success',
+        result: tours.length,
+        data: {
+            tours
+        }
+    })
+}
+
+const getTour = (req, res) => {
+    res.status(200).json({
+        status: 'success',
+        result: tours.length,
+        data: {
+            tours
+        }
+    })
+}
+
+const createTour = (req, res) => {
+    console.log(req.body)
+    res.send('done')
+}
+
+app.route('/api/v1/tours').get(getAllTours).post(createTour)
+app.get('/api/v1/tours/:id', getTour)
 
 const port = 3000
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`server is runnning on ${port}`)
 })
