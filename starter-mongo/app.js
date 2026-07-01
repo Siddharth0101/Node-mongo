c30176792b97c9c8c947086f6b7bc0d323aaee0f
Siddharth0101 <siddsikarwar2001@gmail.com>
2026-07-01 15:30:24 +0530

    init eslist and other changes

--- starter-mongo/app.js
+++ starter-mongo/app.js
@@ -1,49 +1,20 @@
-const express = require('express')
-const fs = require('fs')
-const morgan = require('morgan')
-const app = express()
+const express = require('express');
+const morgan = require('morgan');
+const tourRouter = require('./routes/tourRoutes');
 
-app.use(morgan('dev'))
+const app = express();
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 // express.json() incoming JSON request body ko parse karta hai taaki req.body use kar sakein
-app.use(express.json())
+app.use(express.json());
 
 // app.get('/',(req,res)=>{
 //     res.status(200).json({status:'success',message:'hello from the server'})
 // })
 
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
+app.use('/api/v1/tours', tourRouter);
 
-const getAllTours = (req, res) => {
-    res.status(200).json({
-        status: 'success',
-        result: tours.length,
-        data: {
-            tours
-        }
-    })
-}
-
-const getTour = (req, res) => {
-    res.status(200).json({
-        status: 'success',
-        result: tours.length,
-        data: {
-            tours
-        }
-    })
-}
-
-const createTour = (req, res) => {
-    console.log(req.body)
-    res.send('done')
-}
-
-app.route('/api/v1/tours').get(getAllTours).post(createTour)
-app.get('/api/v1/tours/:id', getTour)
-
-const port = 3000
-
-app.listen(port, () => {
-    console.log(`server is runnning on ${port}`)
-})
+module.exports = app;
---
+++ starter-mongo/controllers/tourController.js
@@ -0,0 +1,55 @@
+const fs = require('fs');
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`),
+);
+
+exports.checkID = (req, res, next, val) => {
+  const id = val * 1;
+  const tour = tours.find((el) => el.id === id);
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+  next();
+};
+
+exports.checkBody = (req, res, next) => {
+  const { name, price } = req.body;
+  if (!name || !price) {
+    return res.status(400).json({
+      status: 'fail',
+      message: 'Missing name or price',
+    });
+  }
+  next();
+};
+
+exports.getAllTours = (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    result: tours.length,
+    data: {
+      tours,
+    },
+  });
+};
+
+exports.getTour = (req, res) => {
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+};
+
+exports.createTour = (req, res) => {
+  console.log(req.body);
+  res.send('done');
+};
