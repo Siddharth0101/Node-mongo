599207c2491023db87b6b67bcb8065360cf09b38
Siddharth0101 <siddsikarwar2001@gmail.com>
2026-07-07 12:49:48 +0530

    added packages

--- starter-mongo/server.js
+++ starter-mongo/server.js
@@ -1,8 +1,29 @@
 const dotenv = require('dotenv');
-
+const mongoose = require('mongoose')
 dotenv.config({ path: './config.env' });
-
 const app = require('./app');
+
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD)
+mongoose.connect(DB)
+  .then(() => console.log('DB connection successful!'));
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true
+  },
+  rating: {
+    type: Number,
+    default: 4.5
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price']
+  }
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
